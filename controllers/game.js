01287ea0aefd3992193e65cb13e8a53c4d3ddd0a
Lewis-Ho <yiucheung.ho@gmail.com>
2015-11-15 01:17:09 -0500

    add circular timer bar

--- controllers/game.js
+++ controllers/game.js
@@ -46,15 +46,40 @@ function resizeDiv(window, array) {
   return (window-(Math.sqrt(array.length)*25))/Math.sqrt(array.length);
 }
 
-app.controller('GameCtrl', ['$scope', '$window', 'scoreboard', function($scope, $window, scoreboard) {
+app.controller('GameCtrl', ['$scope', '$window', '$interval', 'scoreboard', function($scope, $window, $interval, scoreboard) {
   $scope.score = scoreboard.getScore();
   $scope.round = scoreboard.getRound();
-}]);
+  var self = $scope,  j= 0, counter = 0;
 
-//app.controller('ScoreboardCtrl', ['$scope', '$window', 'scoreboard', function($scope, $window, scoreboard) {
-////  $scope.score = scoreboard.getScore();
-////  $scope.round = scoreboard.getRound();
-//}]);
+  self.modes = [ ];
+  self.activated = true;
+  self.time = 0;
+
+  /**
+   * Turn off or on the 5 themed loaders
+   */
+  self.toggleActivation = function() {
+      if ( !self.activated ) self.modes = [ ];
+      if (  self.activated ) j = counter = 0;
+  };
+
+  // Iterate every 100ms, non-stop
+  $interval(function() {
+    // Increment the Determinate loader
+    self.time += 1;
+    if (self.time > 100) {
+      self.time = 0;
+    }
+
+    // Incrementally start animation the five (5) Indeterminate,
+    // themed progress circular bars
+//    if ( (j < 5) && !self.modes[j] && self.activated ) {
+//      self.modes[j] = 'indeterminate';
+//    }
+//    if ( counter++ % 4 == 0 ) j++;
+
+  }, 100, 0, true);
+}]);
 
 app.controller('GameBoardCtrl', ['$scope', '$window', 'scoreboard', function($scope, $window, scoreboard) {
     // Init Setting
@@ -64,8 +89,6 @@ app.controller('GameBoardCtrl', ['$scope', '$window', 'scoreboard', function($sc
     var vph = $window.innerHeight;
     var btnSize = 0;
     $scope.boxes = [];
-//    $scope.$parent.score = scoreboard.getScore();
-//    $scope.round = scoreboard.getRound();
   
     for(var i=0; i<4; i++){
       var newBox = new box(i, 0, ranColor[0]);
